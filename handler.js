02fa516011b7984b81031edc10204f71b1e16854
Yasser Nascimento <yasser.nascimento@nutes.uepb.edu.br>
2021-01-15 18:43:05 -0300

    use puppeteer

--- handler.js
+++ handler.js
@@ -1,8 +1,16 @@
 "use strict";
 
+const chromium = require("chrome-aws-lambda");
+
 module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello Function" }),
-  };
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://ramdajs.com");
+  const content = await page.evaluate(() => document.body.innerHTML);
+
+  return { statusCode: 200, body: JSON.stringify(content) };
 };
