7b070147ed8f1917d4217f77c85f777e3608a2b4
Yasser Nascimento <yasser.nascimento@nutes.uepb.edu.br>
2021-01-15 11:39:15 -0300

    use proxy

--- handler.ts
+++ handler.ts
@@ -2,6 +2,8 @@ import * as serverless from "serverless-http";
 import * as express from "express";
 
 const app = express();
-app.get("/test", (req, res) => res.send({ message: "Testing" }));
+app.get("/test/a", (req, res) => res.send({ message: "Test A" }));
+app.get("/test/b", (req, res) => res.send({ message: "Test B" }));
+app.get("/home", (req, res) => res.send({ message: "Home" }));
 
 exports.app = serverless(app);
