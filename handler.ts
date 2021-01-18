8bc9f5384ed4404155db12a38cbd0e28d8fae47c
Yasser Nascimento <yassern@protonmail.com>
2021-01-18 17:08:35 -0300

    try puppeteer + ts

--- handler.ts
+++ handler.ts
@@ -1,9 +1,22 @@
 import * as serverless from "serverless-http";
 import * as express from "express";
 
+import chromium = require("chrome-aws-lambda");
+
 const app = express();
 app.get("/test/a", (req, res) => res.send({ message: "Test A" }));
 app.get("/test/b", (req, res) => res.send({ message: "Test B" }));
-app.get("/home", (req, res) => res.send({ message: "Home" }));
+app.get("/puppeteer", async (req, res) => {
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://ramdajs.com");
+  const content = await page.evaluate(() => document.body.innerHTML);
+
+  res.send({ message: content });
+});
 
 exports.app = serverless(app);
